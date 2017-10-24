cf1d23b88312f6951c40c38305f0d5d94e88df4d
Oren Koriat <orenk@lacoon.com>
2017-10-24 13:39:51 +0300

    office script

---
+++ office.sh
@@ -0,0 +1,6 @@
+sudo apt-get install wine
+wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add –
+sudo wget http://deb.playonlinux.com/playonlinux_precise.list -O /etc/apt/sources.list.d/playonlinux.list
+sudo apt-get update
+sudo apt-get install playonlinux
+
