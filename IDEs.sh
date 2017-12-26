704df2e193406f3aa475af7ae0866dbf3f10c15d
Oren Koriat <orenk@lacoon.com>
2017-12-26 12:23:34 +0200

    updated repository for Intellij

--- IDEs.sh
+++ IDEs.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository ppa:mmk2410/intellij-idea-community
+sudo add-apt-repository https://launchpad.net/~mmk2410/+archive/ubuntu/intellij-idea
 sudo apt-get update
 sudo apt-get install intellij-idea-community
 sudo add-apt-repository ppa:mystic-mirage/pycharm
