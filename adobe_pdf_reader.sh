d7be7be65d5cdac9066e2063ae69feba776ec83f
GrossBetruger <koriatoren@gmail.com>
2018-02-08 18:18:18 +0200

    added adobe reader

---
+++ adobe_pdf_reader.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository "deb http://archive.canonical.com/ precise partner"
+sudo apt-get update
+sudo apt install adobereader-enu
+sudo add-apt-repository -r "deb http://archive.canonical.com/ precise partner"
+sudo apt update
+
